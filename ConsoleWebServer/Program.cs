1994bc018223bbca32fa3f5a89b7d2ae787e853b
alexs <alex.sircu@gmail.com>
2025-03-05 02:07:30 +0100

    Add project files.

---
+++ ConsoleWebServer/Program.cs
@@ -0,0 +1,13 @@
+﻿using WebServer;
+
+namespace ConsoleWebServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Server.Start();
+            Console.ReadLine();
+        }
+    }
+}
